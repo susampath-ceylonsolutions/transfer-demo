24eadb4553ce4217c642ceb42214ef0c9f080314
Gayan Virajith <gayanvirajith@gmail.com>
2021-03-02 11:59:54 +0530

    initial repository

---
+++ build/precache-manifest.c24e3c2aae5fcab2f248e92ac67ddab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a696882ea029343d3ebf9944354068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa88fdaaa1ce7fb574d2",
+    "url": "/static/css/main.202d3221.chunk.css"
+  },
+  {
+    "revision": "50f28b024c805c56991c",
+    "url": "/static/js/2.4ef30a4d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4ef30a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa88fdaaa1ce7fb574d2",
+    "url": "/static/js/main.d38d95a1.chunk.js"
+  },
+  {
+    "revision": "02aa2995b066c76b8692",
+    "url": "/static/js/runtime-main.ecbdb1ac.js"
+  }
+]);
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.css'
+
+export default function App() {
+    return (
+        <div>
+            Hello app
+        </div>
+    )
+}
